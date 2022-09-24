7378b347be91ca752b515cdbce368e67def84d77
cpulcini2003 <64283587+cpulcini2003@users.noreply.github.com>
2022-09-24 17:12:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+ 
+//make way for some custom css, js and images
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));
+
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
 });
