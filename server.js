1ac25c35adca39062c61637a9e6aec3d9fbeb424
cpulcini2003 <64283587+cpulcini2003@users.noreply.github.com>
2022-09-24 17:06:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-var express = require("express");
- 
-var app = express();
- 
-app.use(express.static('public'));
- 
-//make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
- 
-var server = app.listen(8081, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
 });
 
+app.get('/health', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
